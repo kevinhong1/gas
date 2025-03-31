25862332421fa01b5108d1ba70c44d8d8618992c
kevinhong1 <kevin.hongyu@gmail.com>
2025-03-31 14:35:34 -0800

    Update DesignWorkPage.jsx

--- src/pages/DesignWorkPage/DesignWorkPage.jsx
+++ src/pages/DesignWorkPage/DesignWorkPage.jsx
@@ -12,7 +12,7 @@ const DesignWorkPage = () => {
           content: [
             "Hello, my name is Kevin, and I am a student and web developer based in Port Jefferson, New York.",
             "I have a strong interest in web development, engineering, and finance, and hope to combine my interests to create innovative digital solutions for my clients.",
-            "For me, I love building things, solving problems, and the financial side of the world, whether it’s in code, machines, or the world of money. For me, it’s all about learning, constantly improving, and finding creative methods for solutions. And I’m always excited to take on new challenges and see where they lead.",
+            "For me, I love building things, solving problems, and the financial side of the world, whether it’s in code, machines, or the world of money. For me, it’s all about learning, constantly improving, and finding creative methods for solutions. And I’m always excited to take on new challenges and see where they lead. If you have any inquiries, feel free to email me at kevin.hongyu@gmail.com, or call me at (631) 590-0209 at any time!",
           ],
         }}
         imageSrc={"/images/design.webp"}
